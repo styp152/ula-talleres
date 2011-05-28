14788eac054ad7f717dd520ffb86730155a7d822
Milton Mazzarri <milmazz@gmail.com>
2011-05-28 10:25:23 -0430

    Inclusión de modelos en la aplicación

--- eisula/proyecto/admin.py
+++ eisula/proyecto/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from eisula.proyecto.models import Tesis, Tutor 
+from eisula.proyecto.models import ProyectoDeGrado, Tutor, Autor, \
+                                   PalabraClave, Materia, Biblioteca
 
-admin.site.register(Tesis)
+admin.site.register(ProyectoDeGrado)
 admin.site.register(Tutor)
+admin.site.register(Autor)
+admin.site.register(PalabraClave)
+admin.site.register(Materia)
+admin.site.register(Biblioteca)
