ed72874e59862800a01a2b508cd03a53054af43c
Milton Mazzarri <milmazz@gmail.com>
2011-05-28 11:12:24 -0430

    Ajuste de modelos a visualizar en la interfaz administrativa

--- eisula/proyecto/admin.py
+++ eisula/proyecto/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from eisula.proyecto.models import ProyectoDeGrado, Tutor, Autor, \
-                                   PalabraClave, Materia, Biblioteca, \
-                                   Jurado
+from eisula.proyecto.models import ProyectoDeGrado, Persona, \
+                                   PalabraClave, Materia, Biblioteca
 
-admin.site.register(ProyectoDeGrado)
-admin.site.register(Tutor)
-admin.site.register(Autor)
+admin.site.register(Persona)
 admin.site.register(PalabraClave)
 admin.site.register(Materia)
 admin.site.register(Biblioteca)
-admin.site.register(Jurado)
+admin.site.register(ProyectoDeGrado)
