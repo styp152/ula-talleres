68cc25ce0e162b7114cdbfe2776991e7d81a498b
Milton Mazzarri <milmazz@gmail.com>
2011-05-28 10:02:31 -0430

    Mejoras en los modelos de la aplicación Proyecto
     * Se renombra la clase `Tesis` a `ProyectoDeGrado`
     * Se incluyen nuevos atributos para el modelo `Tesis`
     * Nuevas clases: `PalabraClave`, `Biblioteca`, `Autor` y `Materia`

--- eisula/proyecto/models.py
+++ eisula/proyecto/models.py
@@ -14,19 +14,77 @@ class Tutor(models.Model):
     cedula = models.CharField("cédula", max_length=9, unique=True)
     fecha_nacimiento = models.DateField("fecha de nacimiento", blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = "Tutores"
+
     def __unicode__(self):
         return "%s, %s" % (self.apellido, self.nombre)
 
-class Tesis(models.Model):
+class Autor(models.Model):
+    """
+    Estudiante
+    """
+    nombre = models.CharField(max_length=32)
+    apellido = models.CharField(max_length=32)
+    cedula = models.CharField(max_length=9, unique=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+
+    def __unicode__(self):
+        return "%s, %s" % (self.apellido, self.nombre)
+
+class PalabraClave(models.Model):
+    """
+    Descriptores o palabras claves asociadas al tema del Proyecto de Grado
+    """
+    etiqueta = models.CharField(max_length=32)
+    descripcion = models.TextField("descripción", blank=True)
+
+    class Meta:
+        verbose_name_plural = "Palabras claves"
+
+    def __unicode__(self):
+        return self.etiqueta
+
+class Materia(models.Model):
+    """
+    Materias asociadas al tema de Proyecto de Grado
+    """
+    titulo = models.CharField(max_length=32)
+    descripcion = models.TextField("descripción", blank=True)
+
+    def __unicode__(self):
+        return self.titulo
+
+class Biblioteca(models.Model):
+    """
+    Biblioteca donde se encuentra la publicación asociada al Proyecto de Grado
+    """
+    nombre = models.CharField(max_length=32)
+    direccion = models.TextField("dirección", blank=True)
+
+    def __unicode__(self):
+        return self.nombre
+
+class ProyectoDeGrado(models.Model):
     """
     Proyecto de Grado del estudiante
     """
-    tutor = models.ForeignKey(Tutor)
+    tutor = models.ManyToManyField(Tutor)
     titulo = models.CharField("título", max_length=32)
-    autor = models.CharField(max_length=32)
+    autor = models.OneToOneField(Autor)
+    cota = models.CharField(max_length=16)
     resumen = models.TextField()
-    palabras_claves = models.CharField(max_length=64)
+    fecha_publicacion = models.DateField("fecha de publicación", blank=True,
+                                         null=True)
+    palabra_clave = models.ManyToManyField(PalabraClave)
     enlace_repositorio = models.URLField()
+    nota = models.PositiveIntegerField(blank=True)
+    observacion = models.TextField("observación", blank=True)
+    materia = models.ManyToManyField(Materia, blank=True)
+    biblioteca = models.ForeignKey(Biblioteca)
     
+    class Meta:
+        verbose_name_plural = "Proyectos de Grado"
+
     def __unicode__(self):
-        return self.autor
+        return self.titulo
