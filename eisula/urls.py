a8bc7fa0e3b467f0530a4c005faba5e806ad21ca
Milton Mazzarri <milmazz@gmail.com>
2011-06-04 17:28:21 -0430

    Despliegue el modo desarrollo de medios estáticos

--- eisula/urls.py
+++ eisula/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,3 +14,11 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            url(r'^m/(?P<path>.*)$',
+                                'django.views.static.serve', {'document_root':
+                                                              settings.MEDIA_ROOT,
+                                                             }),
+                           )
