c7a8c4a06dacd65ccd538f7a134108afd828f5fa
Milton Mazzarri <milmazz@gmail.com>
2011-06-04 17:33:33 -0430

    Se incluye el manejo de la vista de detalles del estudiante.

--- eisula/proyecto/urls.py
+++ eisula/proyecto/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import patterns, url
 from eisula.proyecto.views import IndexView, CourseView, ProfessorView, \
-                                  AuthorView, LibraryView
+                                  AuthorView, AuthorDetailView, LibraryView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view()),
-    url(r'^materia/$', CourseView.as_view()),
-    url(r'^profesor/$', ProfessorView.as_view()),
-    url(r'^autor/$', AuthorView.as_view()),
-    url(r'^biblioteca/$', LibraryView.as_view()),
+    url(r'^materias/$', CourseView.as_view()),
+    url(r'^profesores/$', ProfessorView.as_view()),
+    url(r'^autores/(?P<pk>\d+)/$', AuthorDetailView.as_view()),
+    url(r'^autores/$', AuthorView.as_view()),
+    url(r'^bibliotecas/$', LibraryView.as_view()),
 )
