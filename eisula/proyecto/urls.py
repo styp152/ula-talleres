412402da1a6c6e92e7cf9d47e90ea5d030d1b4f6
Milton Mazzarri <milmazz@gmail.com>
2011-06-04 21:58:31 -0430

    Agregado el manejo de las vistas para el detalle de: Cursos, Profesores, Bibliotecas

--- eisula/proyecto/urls.py
+++ eisula/proyecto/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls.defaults import patterns, url
-from eisula.proyecto.views import IndexView, CourseView, ProfessorView, \
-                                  AuthorView, AuthorDetailView, LibraryView
+from eisula.proyecto.views import IndexView, CourseView, CourseDetailView, \
+                                  ProfessorView, ProfessorDetailView, \
+                                  AuthorView, AuthorDetailView, \
+                                  LibraryView, LibraryDetailView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view()),
+    url(r'^materias/(?P<pk>\d+)/$', CourseDetailView.as_view()),
     url(r'^materias/$', CourseView.as_view()),
+    url(r'^profesores/(?P<pk>\d+)/$', ProfessorDetailView.as_view()),
     url(r'^profesores/$', ProfessorView.as_view()),
     url(r'^autores/(?P<pk>\d+)/$', AuthorDetailView.as_view()),
     url(r'^autores/$', AuthorView.as_view()),
+    url(r'^bibliotecas/(?P<pk>\d+)/$', LibraryDetailView.as_view()),
     url(r'^bibliotecas/$', LibraryView.as_view()),
 )
--- eisula/proyecto/views.py
+++ eisula/proyecto/views.py
@@ -5,20 +5,20 @@ from eisula.proyecto.models import ProyectoDeGrado, Estudiante, Profesor, \
 
 class IndexView(ListView):
     queryset = ProyectoDeGrado.objects.order_by("-fecha_publicacion")
-    context_object_name="project_list"
-    #template_name="proyecto/proyectodegrado_list.html"
+    context_object_name = "project_list"
+    #template_name = "proyecto/proyectodegrado_list.html"
 
 
 class AuthorView(ListView):
     model = Estudiante # shorthand: ProyectoDeGrado.objects.all()
-    context_object_name="author_list"
-    #template_name="proyecto/estudiante_list.html"
+    context_object_name = "author_list"
+    #template_name = "proyecto/estudiante_list.html"
 
 
 class AuthorDetailView(DetailView):
     model = Estudiante # = Estudiante.objects.filter(id=pk)
-    context_object_name="author"
-    #template_name="proyecto/estudiante_detail.html"
+    context_object_name = "author"
+    #template_name = "proyecto/estudiante_detail.html"
 
     def get_context_data(self, **kwargs):
         # Llamado a la implementacion base para obtener el contexto
@@ -30,17 +30,38 @@ class AuthorDetailView(DetailView):
 
 class ProfessorView(ListView):
     model = Profesor
-    context_object_name="professor_list"
-    #template_name="proyecto/profesor_list.html"
+    context_object_name = "professor_list"
+    #template_name = "proyecto/profesor_list.html"
+
+
+class ProfessorDetailView(DetailView):
+    model = Profesor
+    context_object_name = "professor"
+    #template_name = "proyecto/profesor_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfessorDetailView, self).get_context_data(**kwargs)
+        context['projects'] = ProyectoDeGrado.objects.filter(tutor=self.object)
+        return context
 
 
 class CourseView(ListView):
     model = Materia
-    context_object_name="course_list"
-    #template_name="proyecto/materia_list.html"
+    context_object_name = "course_list"
+    #template_name = "proyecto/materia_list.html"
+
+
+class CourseDetailView(DetailView):
+    model = Materia
+    context_object_name = "course"
 
 
 class LibraryView(ListView):
     model = Biblioteca
-    context_object_name="library_list"
-    #template_name="proyecto/biblioteca_list.html"
+    context_object_name = "library_list"
+    #template_name = "proyecto/biblioteca_list.html"
+
+
+class LibraryDetailView(DetailView):
+    model = Biblioteca
+    context_object_name = "library"
